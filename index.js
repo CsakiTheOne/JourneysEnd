16da4433e1ee6b74f09fd93d5b1208916ac5663a
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2020-06-12 19:06:01 +0200

    New quest and new system

--- index.js
+++ index.js
@@ -1,53 +1,38 @@
-function $(id) { return document.getElementById(id); }
-
-$('btn1').addEventListener('click', quest1_Complete);
-$('btn2').addEventListener('click', quest2_Complete);
 $('btnReset').addEventListener('click', reset);
+$('btnWitch').addEventListener('click', questWitch_Complete);
+$('btnRainbow').addEventListener('click', questRainbow_Complete);
+$('btnPotion').addEventListener('click', questPotion_Complete);
 
-function quest1_Complete() {
-    if ($('btn1').innerText == 'KÉSZ')
-    {
-        window.location = 'https://youtu.be/-Z4jx5VMw8M';
-        return;
-    }
-
-    let ans = prompt('Vége van. A boszorkány soha nem fog megbántani már. Innentől a béke a jelszó.');
-
-    if (ans != 'béke') return;
-
-    $('btn1').innerText = 'KÉSZ';
-
-    localStorage.setItem('quest1', 'KÉSZ');
-
-    window.location = 'https://youtu.be/-Z4jx5VMw8M';
+function questWitch_Complete() {
+    doQuest('Witch', 'Vége van. A boszorkány soha nem fog megbántani már. Innentől a béke a jelszó.', 'béke', 'https://youtu.be/-Z4jx5VMw8M');
 }
 
-function quest2_Complete() {
-    if ($('btn1').innerText != 'KÉSZ')
+function questRainbow_Complete() {
+    if ($('btnWitch').innerText != 'KÉSZ')
     {
         alert('Még meg kell csinálnod az "Utolsó főellenség" küldetést!');
         return;
     }
-    if ($('btn2').innerText == 'KÉSZ')
+    doQuest('Rainbow', 'Milyen a természet, ha színes? (1 szó)', 'Wonderful', 'https://youtu.be/knGhzlGsGtk');
+}
+
+function questPotion_Complete() {
+    if ($('btnWitch').innerText != 'KÉSZ')
     {
-        alert('Ez a küldetés már kész van!');
+        alert('Még meg kell csinálnod az "Utolsó főellenség" küldetést!');
         return;
     }
-
-    let ans = prompt('Mi volt a palack eredeti tartalma?');
-
-    if (ans != 'Potion of Regeneration') return;
-
-    $('btn2').innerText = 'KÉSZ';
-    
-    localStorage.setItem('quest2', 'KÉSZ');
+    doQuest('Potion', 'Mi volt a palack eredeti tartalma?', 'Potion of Regeneration');
 }
 
-function reset() {
-    localStorage.removeItem('quest1');
-    localStorage.removeItem('quest2');
-    window.location.reload();
+function doQuest(questName, question, answer, reward) {
+    if ($('btn' + questName).innerText == 'KÉSZ') window.location = reward;
+    let ans = prompt(question);
+    if (ans != answer)
+    {
+        alert('Nem sikerült teljesíteni a küldetést.');
+    }
+    $('btn' + questName).innerText = 'KÉSZ';
+    localStorage.setItem('quest' + questName, 'KÉSZ');
+    window.location = reward;
 }
-
-if (localStorage.getItem('quest1') == 'KÉSZ') $('btn1').innerText = 'KÉSZ';
-if (localStorage.getItem('quest2') == 'KÉSZ') $('btn2').innerText = 'KÉSZ';
