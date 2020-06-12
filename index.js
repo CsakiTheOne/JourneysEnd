d7fc7566791f72692f80c9d2b823028eb1546bb3
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2020-06-12 16:51:40 +0200

    Quest functional

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function $(id) { return document.getElementById(id); }
 
 $('btn1').addEventListener('click', quest1_Complete);
 $('btn2').addEventListener('click', quest2_Complete);
+$('btnReset').addEventListener('click', reset);
 
 function quest1_Complete() {
     if ($('btn1').innerText == 'KÉSZ')
@@ -10,12 +11,13 @@ function quest1_Complete() {
         return;
     }
 
-    alert('Még nem tudod teljesíteni ezt a küldetést. Próbáld meg később!');
-    return;
+    let ans = prompt('Vége van. A boszorkány soha nem fogja megkeseríteni az életed. Innentől a béke a jelszó.');
 
-    let ans = prompt();
+    if (ans != 'béke') return;
 
-    $('btn1').innerText('KÉSZ');
+    $('btn1').innerText = 'KÉSZ';
+
+    localStorage.setItem('quest1', 'KÉSZ');
 }
 
 function quest2_Complete() {
@@ -30,10 +32,20 @@ function quest2_Complete() {
         return;
     }
 
-    alert('Még nem tudod teljesíteni ezt a küldetést. Próbáld meg később!');
-    return;
-
     let ans = prompt('Mi volt a palack eredeti tartalma?');
 
-    $('btn2').innerText('KÉSZ');
+    if (ans != 'Potion of Regeneration') return;
+
+    $('btn2').innerText = 'KÉSZ';
+    
+    localStorage.setItem('quest2', 'KÉSZ');
 }
+
+function reset() {
+    localStorage.removeItem('quest1');
+    localStorage.removeItem('quest2');
+    window.location.reload();
+}
+
+if (localStorage.getItem('quest1') == 'KÉSZ') $('btn1').innerText = 'KÉSZ';
+if (localStorage.getItem('quest2') == 'KÉSZ') $('btn2').innerText = 'KÉSZ';
