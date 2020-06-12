8264449f9626664e8b8f938a75d29de4a0579eec
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2020-06-12 19:18:19 +0200

    Removed helper file

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// HELPER
+
+function $(id) { return document.getElementById(id); }
+
+function reset() {
+    localStorage.removeItem('questWitch');
+    localStorage.removeItem('questRainbow');
+    localStorage.removeItem('questPotion');
+    window.location.reload();
+}
+
+if (localStorage.getItem('questWitch') == 'KÉSZ') $('btnWitch').innerText = 'KÉSZ';
+if (localStorage.getItem('questRainbow') == 'KÉSZ') $('btnRainbow').innerText = 'KÉSZ';
+if (localStorage.getItem('questPotion') == 'KÉSZ') $('btnPotion').innerText = 'KÉSZ';
+
+// INDEX
+
 $('btnReset').addEventListener('click', reset);
 $('btnWitch').addEventListener('click', questWitch_Complete);
 $('btnRainbow').addEventListener('click', questRainbow_Complete);
